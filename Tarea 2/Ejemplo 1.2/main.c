7c0990ef298cae64156514e9ac29ff58ca73d43d
Josarys <diazjosaris0@gmail.com>
2026-05-28 15:25:53 -0400

    Organizando tarea 2, ejemplo 1.2

--- Tarea 2/Ejemplo 1.2/main.c
+++ Tarea 2/Ejemplo 1.2/main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
+<<<<<<< HEAD
 
 /*Promedio curso. El programa recibe el promedio del alumno. Si su promedio es mayor o igual a 6 escriibe aprobado.
 
+=======
+#include <stdlib.h>
+
+/*Promedio curso. El programa, al recibir como dato un promedio de un alumno universitario, escribe aprobado si su promedio es mayor a 6.
+>>>>>>> 9d5c0ed (Organizando tarea 2, ejemplo 1.2)
 PRO: variable de tipo real. */
 
 void main(void)
 {
     float PRO;
+<<<<<<< HEAD
     printf("Ingrese el promedio del alumno: ");
     scanf("%f", &PRO);
     if (PRO >= 6)
         printf("\nAprobado");
+=======
+    printf("Ingrese el promedio del alumno:");
+    scanf("%f", &PRO);
+    if (PRO >= 6)
+        printf("\nAprovado");
+>>>>>>> 9d5c0ed (Organizando tarea 2, ejemplo 1.2)
 }
